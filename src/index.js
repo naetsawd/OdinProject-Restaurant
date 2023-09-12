73987357edcfcf6d5cb3be02e9c1a317ffc27efb
naetsawd <dechsitbkk@gmail.com>
2023-09-11 22:02:04 -0400

    Setup the starting structure of the website.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// function component() {
+//     const element = document.createElement('div');
+//     element.innerHTML = "Hello World!";
+    
+//     return element;
+// }
+  
+// document.body.appendChild(component());
+
+const element = document.getElementById("content");
+element.innerHTML = "Hello World!";
