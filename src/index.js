68b877d5dad970790735112de0bb1b4145a05c94
naetsawd <dechsitbkk@gmail.com>
2023-09-13 21:28:26 -0400

    Revamped the hardcoded theme and layout

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-// function component() {
-//     const element = document.createElement('div');
-//     element.innerHTML = "Hello World!";
+function makeHeader() {
+    document.createElement("header");
     
-//     return element;
-// }
-  
-// document.body.appendChild(component());
+}
 
-// const element = document.getElementById("content");
-// element.innerHTML = "Hello World!";
+function makeHome() {
+
+}
