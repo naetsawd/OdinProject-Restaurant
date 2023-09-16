a5b3e86f9cf30fafd7ea803384060962c1649bb2
naetsawd <dechsitbkk@gmail.com>
2023-09-15 20:00:45 -0400

    Transferred hardcoded HTML into JavaScript to create elements dynamically.

---
+++ src/home.js
@@ -0,0 +1,25 @@
+export function createHomeSection() {
+    const homeContainer = document.createElement('span');
+    homeContainer.id = 'home';
+
+    const homeTitle = document.createElement('div');
+    homeTitle.id = 'homeTitle';
+    homeTitle.textContent = 'Level Up Your Nostalgia!';
+
+    const whiteLine = document.createElement('div');
+    whiteLine.classList.add('whiteLine');
+
+    const homeDesc = document.createElement('div');
+    homeDesc.id = 'homeDesc';
+    homeDesc.textContent = 'Pixel Bites is a nostalgic dining destination that ' +
+        'transports you back to the golden age of video games. With ' +
+        'pixelated decor, classic arcade games, and a menu inspired ' +
+        'by iconic gaming characters, this diner is a haven for gamers ' +
+        'and retro enthusiasts alike.';
+
+    homeContainer.appendChild(homeTitle);
+    homeContainer.appendChild(whiteLine);
+    homeContainer.appendChild(homeDesc);
+
+    return homeContainer;
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-function makeHeader() {
-    document.createElement("header");
-    
+import { createHomeSection } from "./home";
+
+const grid = document.getElementById("grid");
+
+function createHeader() {
+    const header = document.createElement("header");
+    const nav = document.createElement("nav");
+
+    const menuLink = document.createElement("p");
+    menuLink.textContent = "Menu";
+
+    const logo = document.createElement("p");
+    logo.textContent = "Pixel Bites";
+    logo.id = "navLogo";
+
+    const contactLink = document.createElement("p");
+    contactLink.textContent = "Contact";
+
+    nav.appendChild(menuLink);
+    nav.appendChild(logo);
+    nav.appendChild(contactLink);
+
+    header.appendChild(nav);
+
+    return header;
 }
 
-function makeHome() {
+grid.appendChild(createHeader());
 
-}
+grid.appendChild(createHomeSection());
